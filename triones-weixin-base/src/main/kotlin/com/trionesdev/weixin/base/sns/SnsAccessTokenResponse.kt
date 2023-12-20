36e5447b24fe9b6802184ee1b76cf3076790402a
Bane.Shi <fengxiaotx@163.com>
2023-12-20 16:51:12 +0800

    feat:

--- triones-weixin-base/src/main/kotlin/com/trionesdev/weixin/base/sns/SnsAccessTokenResponse.kt
+++ triones-weixin-base/src/main/kotlin/com/trionesdev/weixin/base/sns/SnsAccessTokenResponse.kt
@@ -1,8 +1,10 @@
 package com.trionesdev.weixin.base.sns
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.trionesdev.weixin.base.model.BaseResponse
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class SnsAccessTokenResponse : BaseResponse() {
     @JsonProperty(value = "access_token")
     var accessToken: String? = null
@@ -16,4 +18,7 @@ class SnsAccessTokenResponse : BaseResponse() {
     @JsonProperty(value = "openid")
     var openId: String? = null
     var scope: String? = null
+
+    @JsonProperty(value = "unionid")
+    var unionId: String? = null
 }
